7fe20d0019516b593cf63541b8da4cdfe22c1574
rezzydoodle <rezzydoodle@84ae0652-ca6a-11de-a70d-3b9dd4243769>
2009-11-12 00:58:21 +0000

    Incomplete code lives here. Gitchu one.

---
+++ incomplete/associate.pl
@@ -0,0 +1,21 @@
+#co-op program Matt & Dylan
+
+sub get_twit_user_info
+{
+	$username = shift; #twiki username
+	@twitterinfo = ''; #put some real data here
+	#get twitter username from storage
+	#should be paired with $username
+	#maybe a password too
+	return @twitterinfo;
+}	
+
+sub get_twik_user_info
+{
+	$username = shift; #twitter username
+	$twikiinfo = ''; #put some real data here
+	#get twiki user info from storage
+	#should be paired with $username
+	return $twikiinfo;
+}	
+
