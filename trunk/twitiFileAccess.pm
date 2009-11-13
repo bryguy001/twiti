401c6c26649b370b48ae10f2d2874c0657aa8aee
anbulen <anbulen@84ae0652-ca6a-11de-a70d-3b9dd4243769>
2009-11-13 01:10:47 +0000

    readFile

---
+++ trunk/twitiFileAccess.pm
@@ -0,0 +1,16 @@
+#! /usr/bin/perl
+
+# first param is the username/filename
+sub retrieveTwitterJunk
+{
+  my $filename = $_[0].".twiti";
+  $text = Twiki::Func::readFile($filename);
+  @textArray = split('\n',$text);
+  return @textArray;  
+}
+
+# returns twiiter information based on current Twiki user
+sub currentUserTwitter
+{
+  return retrieveTwitterJunk(%username.".twiti");
+}
