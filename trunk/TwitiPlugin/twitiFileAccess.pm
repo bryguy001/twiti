85b58556186f33bb11ab034976c835679652d379
mikevh0 <mikevh0@84ae0652-ca6a-11de-a70d-3b9dd4243769>
2009-11-20 01:31:20 +0000

    single file and encryption

--- trunk/TwitiPlugin/twitiFileAccess.pm
+++ trunk/TwitiPlugin/twitiFileAccess.pm
@@ -1,4 +1,7 @@
-#! /usr/bin/perl
+#!/usr/bin/perl
+
+use Twiki::Func;
+use Crypt::Simple passphrase => 'twiti'
 
 # first param is the username/filename
 sub retrieveTwitterJunk
@@ -6,6 +9,7 @@ sub retrieveTwitterJunk
   my $filename = $_[0].".twiti";
   $text = Twiki::Func::readFile($filename);
   @textArray = split('\n',$text);
+  $textArray[1] = decrypt($textArray[1]);
   return @textArray;  
 }
 
@@ -15,4 +19,14 @@ sub currentUserTwitter
   return retrieveTwitterJunk(%username.".twiti");
 }
 
-change here
+#first param is the username/filename
+#second param is Twitter username
+#third param is the Twitter password
+sub StoreUsernameAndPassword
+{
+  my $filename = $_[0] . ".twiti";
+  my $storestring = $store . '\n' . $_[1];
+  $storestring = $store . '\n' . encrypt($_[2]);
+  TWiki::Func::saveFile($filename, $storestring);
+}
+                                
