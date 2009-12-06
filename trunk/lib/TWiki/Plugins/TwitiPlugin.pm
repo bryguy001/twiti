cb095eb494a64749292f4704fb0bfd19b004d894
Tachyon1 <Tachyon1@84ae0652-ca6a-11de-a70d-3b9dd4243769>
2009-12-06 19:53:09 +0000

    Added twitiLogin function that outputs a pretty login screen, changed handleTwiti to return  temporarily to see what the default user shows as

--- trunk/lib/TWiki/Plugins/TwitiPlugin.pm
+++ trunk/lib/TWiki/Plugins/TwitiPlugin.pm
@@ -72,7 +72,15 @@ sub handleTwiti
 	my $webName = $session->{webName};
 	my $topic = $session->{topicName};
 	my $user = $session->{user};
+	return $user;
 	
+	require TWiki::Plugins::TwitiPlugin::twitiFileAccess;
+	if(TWiki::Plugins::TwitiPlugin::twitiFileAccess::currentUserTwitter($user) eq "")
+	{
+		require TWiki::Plugins::TwitiPlugin::Twiti;
+		return TWiki::Plugins::TwitiPlugin::Twiti::twitiLogin(); 
+	}
+
 	if($topic eq 'TwitiPlugin')
 	{  
 		require TWiki::Plugins::TwitiPlugin::Twiti;
