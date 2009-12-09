2b703655e04ffdd7ded1ae12f28239411d1e310c
gamerdonkey <gamerdonkey@84ae0652-ca6a-11de-a70d-3b9dd4243769>
2009-12-09 03:54:31 +0000

    Removed redundant sub in twitiFileAccess

--- lib/TWiki/Plugins/TwitiPlugin/twitiFileAccess.pm
+++ lib/TWiki/Plugins/TwitiPlugin/twitiFileAccess.pm
@@ -2,20 +2,15 @@ package TWiki::Plugins::TwitiPlugin::twitiFileAccess;
 
 #use Crypt::Simple passphrase => 'twiti';
 
-# first param is the username/filename
-sub retrieveTwitterJunk
+# returns twiiter information based on Twiki username
+# passed as a parameter
+sub currentUserTwitter
 {
   my $filename = TWiki::Func::getWorkArea('TwitiPlugin') . "/" . $_[0].".twiti";
   $text = TWiki::Func::readFile($filename);
   @textArray = split(/,/,$text);
   #$textArray[1] = decrypt($textArray[1]);
   return @textArray;  
-}
-
-# returns twiiter information based on current Twiki user
-sub currentUserTwitter
-{
-  return retrieveTwitterJunk($_[0]);
 }
 
 #first param is the username/filename
