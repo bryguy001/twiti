0dbda6f468107bea2ef24f0535126a8bd60fef73
gamerdonkey <gamerdonkey@84ae0652-ca6a-11de-a70d-3b9dd4243769>
2009-12-10 01:47:37 +0000

    try to fix handleTwiti thispage

--- trunk/lib/TWiki/Plugins/TwitiPlugin/Twiti.pm
+++ trunk/lib/TWiki/Plugins/TwitiPlugin/Twiti.pm
@@ -465,7 +465,7 @@ sub twitiPageSpecific
 	my @newStatuses;
 	foreach my $status (@$statuses)
 	{
-		if( $status =~ m/.*($curPageTinyUrl).*/ )
+		if( $status->{text} =~ m/($curPageTinyUrl)/ )
 		{
 			push(@newStatuses, $status);
 		}
