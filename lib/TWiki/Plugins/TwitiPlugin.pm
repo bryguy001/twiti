3e9bfc1195d42a3c5f2f719c5d864efabc0469d2
gamerdonkey <gamerdonkey@84ae0652-ca6a-11de-a70d-3b9dd4243769>
2009-12-10 01:26:16 +0000

    try to fix handleTwiti thispage

--- lib/TWiki/Plugins/TwitiPlugin.pm
+++ lib/TWiki/Plugins/TwitiPlugin.pm
@@ -87,7 +87,7 @@ sub handleTwiti
 	{
 		if($attr)#->{thispage} eq "1")
 		{
-			return $attr;
+			return %$attr;
 			#return TWiki::Plugins::TwitiPlugin::Twiti::twitiPageSpecific($session);
 		}
 		elsif($topic eq 'TwitiPlugin')
